ff2cc58c3c4dd05ca8300fcde3fa11a22e737561
Hanif tuanmiden <hanif19001@gmail.com>
2025-02-27 16:10:24 +0700

    add whishlistspage

---
+++ Client/src/app/Wishlists/page.jsx
@@ -0,0 +1,8 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card.tsx";
+import { Input } from "@/components/ui/input.jsx";
+import { Button } from "@/components/ui/button.jsx";
+import { Label } from "@/components/ui/label.tsx";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar.tsx";
+
+export default function WishlistsPage() {
+}
