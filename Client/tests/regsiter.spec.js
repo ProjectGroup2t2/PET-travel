f762d4c1da4ead2fd1e5b7fc39292cb2cdf1f558
Teerepat Phakdee <teerepatphakdeeaof@gmail.com>
2025-03-09 21:04:26 +0700

    Register play wright test

---
+++ Client/tests/regsiter.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/register');
+  await page.getByRole('textbox', { name: 'USERNAME' }).click();
+  await page.getByRole('textbox', { name: 'USERNAME' }).fill('test');
+  await page.getByRole('textbox', { name: 'EMAIL' }).click();
+  await page.getByRole('textbox', { name: 'EMAIL' }).fill('test@gmail.com');
+  await page.getByRole('textbox', { name: 'PASSWORD', exact: true }).click();
+  await page.getByRole('textbox', { name: 'PASSWORD', exact: true }).fill('123456');
+  await page.getByRole('textbox', { name: 'CONFIRM PASSWORD' }).click();
+  await page.getByRole('textbox', { name: 'CONFIRM PASSWORD' }).fill('123456');
+  await page.getByRole('textbox', { name: 'CONFIRM PASSWORD' }).press('Enter');
+  await page.getByRole('button', { name: 'REGISTER' }).click();
+});
